ad83c4a7d214b7b5b09e703a12be87a06ecc732c
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-07-26 02:25:41 +0700

    Update floweq_2d.py

--- simulators/floweq_2d.py
+++ simulators/floweq_2d.py
@@ -1,52 +1,144 @@
-# blocks location
-block = np.arange(1, 5+1)
+xi = 4; yi = 3 # number of blocks in x and y
+
+# create block coordinates (Engineering Notation convention in Abou-Kassem)
+x_ = np.arange(1, xi+1)
+y_ = np.arange(1, yi+1)
+
+x, y = np.meshgrid(x_, y_, indexing='ij')
+
+# plot grid points
+plt.scatter(x, y)
+
+# Python starts indexing from 0. So, block 1,1 refers to [0,0], block 2,1
+# refers to [1,0] and so on. In other words, block i,j refers to [i-1,j-1]
+
+# print('Block {},{}'.format(x[2,1], y[2,1])) # print block 3,2
 
 # define parameters, in this case blocks are homogeneous and same in size
-dx = 1000 # ft
-dy = 1200
-dz = 75
-kx = 15 # md
+dx = 250 # ft
+dy = 300
+dz = 100
+kx = 270 # md
+ky = 220
 B = 1 # RB/STB
-mu = 10 # cp
-p_grad = -0.1 # psi/ft
+mu = 2 # cp
 
 # assign all params to all blocks
-dx = np.full(len(block), dx)
-dy = np.full(len(block), dy)
-dz = np.full(len(block), dz)
-kx = np.full(len(block), kx)
-B = np.full(len(block), B)
-mu = np.full(len(block), mu)
+dx = np.array([[dx]*yi]*xi)
+dy = np.array([[dy]*yi]*xi)
+dz = np.array([[dz]*yi]*xi)
+kx = np.array([[kx]*yi]*xi)
+ky = np.array([[ky]*yi]*xi)
+B = np.array([[B]*yi]*xi)
+mu = np.array([[mu]*yi]*xi)
 
 # source term
-qsc = np.zeros(len(block)) # initiate with zeros
-qsc[3] = -150 # inject the source to block 4
+xsc = 3; ysc = 2 # grid point (x and y coordinate) of the source 
+q = -4000 
 
-# boundary conditions
-pg_bW = 0.1 # pressure gradient at left (West) boundary
-q_bE = 50. # flow rate at right (East) boundary
+qsc = np.array([[0]*yi]*xi) # initiate with zeros
+qsc[xsc-1,ysc-1] = q # inject the source to block 3,2
 
-print('Left-Hand Side (RHS) of Flow equation in each boundary block')
-print('1st term: Flow in X-direction from adjacent block')
-print('2nd term: Fictitious flow into boundary block')
-print('3rd term: Source accumulation \n')
+# plot the injected grid point
+plt.scatter(x[xsc-1,ysc-1], y[xsc-1,ysc-1], color='red')
 
-print('Left-Hand Side (RHS) of Flow equation in each internal block')
-print('1st term: Flow in X-direction from previous block')
-print('2nd term: Flow in X-direction from next block')
-print('3rd term: Source accumulation \n')
+# boundary blocks
+q_bN = -500 # flow rate at North boundary
+p_bS = 3000 # pressure at South boundary
+q_bW = 0 # flow rate at West boundary
+pg_bE = .1 # pressure gradient at East boundary
 
-for i in range(len(block)):
-  A = dy[i] * dz[i]
-  T_min = .001127 * (kx[i] * A) / (mu[i] * B[i] * dx[i])
-  T_plus = T_min 
-  if i == 0:
-    # constant pressure gradient at left boundary
-    qsc_b = constant_pressuregrad_bc1d('west', p_grad, dy[i], dz[i], kx[i], mu[i], B[i])
-    print('Boundary Block {}: {} (p{} - p{}) + {} + {}'.format(block[i], T_plus, block[i+1], block[i], qsc_b, qsc[i]))
-  elif i == len(block)-1:
-    # specified flow at right boundary
-    qsc_b = constant_rate_bc1d(q_bE)
-    print('Boundary Block {}: {} (p{} - p{}) + {} + {}'.format(block[i], T_min, block[i-1], block[i], qsc_b, qsc[i]))
-  else:
-    print('Interior Block {}: {} (p{} - p{}) + {} (p{} - p{}) + {}'.format(block[i], T_min, block[i-1], block[i], T_plus, block[i+1], block[i], qsc[i]))
+print('Left-Hand Side (RHS) of Flow equation in each block')
+print('1st term: Flow in X-direction from next block (Tx+)')
+print('2nd term: Flow in X-direction from previous block (Tx-)')
+print('3rd term: Flow in Y-direction from next block (Ty+)')
+print('4th term: Flow in Y-direction from previous block (Ty-)')
+print('5th term: Source accumulation \n')
+
+for i in range(xi):
+  for j in range(yi):
+
+    Ax = dy[i,j] * dz[i,j]
+    Ay = dx[i,j] * dz[i,j]
+
+    # flow to x direction
+    Tx_min = .001127 * (kx[i,j] * Ax) / (mu[i,j] * B[i,j] * dx[i,j])
+    Tx_plus = Tx_min
+
+    # flow to y direction
+    Ty_min = .001127 * (ky[i,j] * Ay) / (mu[i,j] * B[i,j] * dy[i,j])
+    Ty_plus = Ty_min 
+
+    if i==0:
+      # boundaries in the West
+      qsc_bW = constant_rate_bc2d(Tx_min, 0, yi)
+
+      if j==0:
+        # southwest boundary has two face boundaries (cons press & no flow)
+        # Tx- and Ty- are flow from boundaries
+        qsc_bS = constant_pressure_bc1d((i+1,j+1), p_bS, dx[i,j], dz[i,j], ky[i,j], dy[i,j], mu[i,j], B[i,j])
+        print('southwest corner block {}'.format((i+1,j+1)))  
+        print('Boundary Block {}: {} (p{} - p{}) + {} + {} (p{} - p{}) + {} + {} \n'.format((i+1,j+1), Tx_plus, (i+2,j+1), (i+1,j+1), qsc_bW, Ty_plus, (i+1,j+2), (i+1,j+1), qsc_bS, qsc[i,j]))         
+
+      if j==yi-1:
+        # northwest boundary has two face boundaries (cons rate & cons press)
+        # Tx- and Ty+ are flow from boundaries
+        qsc_bN = constant_rate_bc2d(Ty_plus, q_bN, xi)
+        print('northwest corner block {}'.format((i+1,j+1)))
+        print('Boundary Block {}: {} (p{} - p{}) + {} + {} + {} (p{} - p{}) + {} \n'.format((i+1,j+1), Tx_plus, (i+2,j+1), (i+1,j+1), qsc_bW, qsc_bN, Ty_min, (i+1,j), (i+1,j+1), qsc[i,j]))         
+
+
+      else:
+        if j!=0 and j!=yi-1:
+          # west boundary has no flow
+          # Tx+ is flow from boundary
+          print('west block {}'.format((i+1,j+1)))   
+          print('Boundary Block {}: {} (p{} - p{}) + {} + {} (p{} - p{}) + {} (p{} - p{}) + {} \n'.format((i+1,j+1), Tx_plus, (i+2,j+1), (i+1,j+1), qsc_bW, Ty_plus, (i+1,j+2), (i+1,j+1), Ty_min, (i+1,j), (i+1,j+1), qsc[i,j]))         
+    
+    elif i==xi-1:
+      # boundaries in the East
+      qsc_bE = constant_pressuregrad_bc1d('east', pg_bE, dy[i,j], dz[i,j], kx[i,j], mu[i,j], B[i,j])
+
+      if j==0:
+        # southeast boundary has two face boundaries (cons press grad & cons press)
+        # Tx+ and Ty- are flow from boundaries
+        qsc_bS = constant_pressure_bc1d((i+1,j+1), p_bS, dx[i,j], dz[i,j], ky[i,j], dy[i,j], mu[i,j], B[i,j])
+        print('southeast corner block {}'.format((i+1,j+1)))
+        print('Boundary Block {}: {} + {} (p{} - p{}) + {} (p{} - p{}) + {} + {} \n'.format(block[i], qsc_bE, Tx_min, (i,j+1), (i+1,j+1), Ty_plus, (i+1,j+2), (i+1,j+1), qsc_bS, qsc[i,j]))
+
+      if j==yi-1:
+        # northeast boundary has two face boundaries (cons press grad & cons rate)
+        # Tx+ and Ty+ are flow from boundaries
+        qsc_bN = constant_rate_bc2d(Ty_plus, q_bN, xi)
+        print('northeast corner block {}'.format((i+1,j+1))) 
+        print('Boundary Block {}: {} + {} (p{} - p{}) + {} + {} (p{} - p{}) + {} \n'.format(block[i], qsc_bE, Tx_min, (i,j+1), (i+1,j+1), qsc_bN, Ty_min, (i+1,j), (i+1,j+1), qsc[i,j]))
+
+      else:
+        if j!=0 and j!=yi-1:
+          # east boundary has constant pressure gradient
+          # Tx- is flow from boundary
+          print('east block {}'.format((i+1,j+1)))  
+          print('Boundary Block {}: {} + {} (p{} - p{}) + {} (p{} - p{}) + {} (p{} - p{}) + {} \n'.format(block[i], qsc_bE, Tx_min, (i,j+1), (i+1,j+1), Ty_plus, (i+1,j+2), (i+1,j+1), Ty_min, (i+1,j), (i+1,j+1), qsc[i,j]))
+
+
+    elif i!=0 and i!=xi-1:
+      if j==0:
+        # south boundary has constant pressure 
+        # Ty- is flow from boundary
+        qsc_bS = constant_pressure_bc1d((i+1,j+1), p_bS, dx[i,j], dz[i,j], ky[i,j], dy[i,j], mu[i,j], B[i,j])
+        print('south block {}'.format((i+1,j+1)))
+        print('Boundary block {}: {} (p{} - p{}) + {} (p{} - p{}) + {} (p{} - p{}) + {} + {} \n'.format((i+1, j+1), Tx_plus, (i, j+1), (i+1, j+1), Tx_min, (i+2, j+1), (i+1, j+1), Ty_plus, (i+1, j+2), (i+1,j+1), qsc_bS, qsc[i,j]))
+
+      if j==yi-1:  
+        # north boundary has constant flow rate
+        # Ty+ is flow from boundary        
+        qsc_bN = constant_rate_bc2d(Ty_plus, q_bN, xi)
+        print('north block {}'.format((i+1,j+1)))           
+        print('Boundary block {}: {} (p{} - p{}) + {} (p{} - p{}) + {} + {} (p{} - p{}) + {} \n'.format((i+1, j+1), Tx_plus, (i, j+1), (i+1, j+1), Tx_min, (i+2, j+1), (i+1, j+1), qsc_bN, Ty_min, (i+1, j), (i+1,j+1), qsc[i,j]))
+
+      else:
+        if j!=0 and j!=yi-1:
+          # the interior blocks
+          # no boundary flow
+          print('interior block {}'.format((i+1,j+1)))
+          print('Interior block {}: {} (p{} - p{}) + {} (p{} - p{}) + {} (p{} - p{}) + {} (p{} - p{}) + {} \n'.format((i+1, j+1), Tx_plus, (i, j+1), (i+1, j+1), Tx_min, (i+2, j+1), (i+1, j+1), Ty_plus, (i+1, j+2), (i+1, j+1), Ty_min, (i+1, j), (i+1,j+1), qsc[i,j]))
