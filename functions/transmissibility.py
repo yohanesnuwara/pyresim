6206cc09556a73a5e6e58564a2827a8fa614cc3c
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-07-29 22:47:48 +0700

    Update transmissibility.py

--- functions/transmissibility.py
+++ functions/transmissibility.py
@@ -4,6 +4,9 @@ def trans_r_min(dz_now, dz_prev, dtheta, alpha_tg, kh_now, kh_prev, mu, B):
   """
   Calculate transmissibility of cylindrical grid in r- direction
   """
+  
+  import numpy as np
+  
   # transmissibility geometric factor
   G_trans_r_min = (.001127 * dtheta) / (np.log(alpha_tg * np.log(alpha_tg) / (alpha_tg - 1)) / (dz_now * kh_now) + np.log((alpha_tg - 1) / np.log(alpha_tg)) / (dz_prev * kh_prev))
   # calculate transmissibility
@@ -14,6 +17,9 @@ def trans_r_plus(dz_now, dz_next, dtheta, alpha_tg, kh_now, kh_next, mu, B):
   """
   Calculate transmissibility of cylindrical grid in r+ direction
   """
+  
+  import numpy as np
+  
   # transmissibility geometric factor
   G_trans_r_plus = (.001127 * dtheta) / (np.log((alpha_tg - 1) / np.log(alpha_tg)) / (dz_now * kh_now) + np.log(alpha_tg * np.log(alpha_tg) / (alpha_tg - 1)) / (dz_next * kh_next))
   # calculate transmissibility
@@ -24,6 +30,7 @@ def trans_z_min(dz_now, dz_prev, dv_now, kz_now, kz_prev, mu, B):
   """
   Calculate transmissibility of cylindrical grid in z- direction
   """
+  
   # transmissibility geometric factor
   G_trans_z_min = (2 * .001127 * (dv_now / dz_now)) / ((dz_now / kz_now) + (dz_prev / kz_prev))
   # calculate transmissibility
@@ -34,6 +41,7 @@ def trans_z_plus(dz_now, dz_next, dv_now, kz_now, kz_next, mu, B):
   """
   Calculate transmissibility of cylindrical grid in z+ direction
   """
+  
   # transmissibility geometric factor
   G_trans_z_plus = (2 * .001127 * (dv_now / dz_now)) / ((dz_now / kz_now) + (dz_next / kz_next))
   # calculate transmissibility
@@ -44,6 +52,9 @@ def trans_theta_min(dz_now, dz_prev, alpha_tg, dtheta_now, dtheta_prev, ktheta_n
   """
   Calculate transmissibility of cylindrical grid in θ- direction
   """
+  
+  import numpy as np
+  
   # transmissibility geometric factor
   G_trans_theta_min = (2 * .001127 * np.log(alpha_tg)) / ((dtheta_now / (dz_now * ktheta_now)) + (dtheta_prev / (dz_prev * ktheta_prev)))
   # calculate transmissibility
@@ -54,6 +65,9 @@ def trans_theta_plus(dz_now, dz_next, alpha_tg, dtheta_now, dtheta_next, ktheta_
   """
   Calculate transmissibility of cylindrical grid in θ+ direction
   """
+  
+  import numpy as np
+  
   # transmissibility geometric factor
   G_trans_theta_plus = (2 * .001127 * np.log(alpha_tg)) / ((dtheta_now / (dz_now * ktheta_now)) + (dtheta_next / (dz_next * ktheta_next)))
   # calculate transmissibility
