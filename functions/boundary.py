eb9dc1a0cc81f00e3c51873cda69f17e28cad3bb
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-07-30 15:49:02 +0700

    Update boundary.py

--- functions/boundary.py
+++ functions/boundary.py
@@ -83,6 +83,60 @@ def constant_pressure_bc2d(no_block, p_b, dy, dz, kx, dx, mu, B):
   qsc = '{} ({} - p{})'.format(T, p_b, no_block)
   return(qsc)
 
+def constant_pressure_bc3d(bound_loc, no_block, p_b, dy, dz, kx, dx, mu, B, rho):
+  """
+  Flow expression for 3D constant pressure boundary block
+
+  Input:
+
+  bound_loc = location of boundary. Specify as 'west', 'east', 'south', 'north', 'bottom', or 'upper'
+  no_block = boundary block location
+  p_b = pressure at the boundary
+  dy, dz = these two can be interchangeable depends on the boundary location
+  * if the boundary block receives constant pressure boundary in y direction: input as dx, dz
+  * if the boundary block receives constant pressure boundary in x direction: input as dy, dz
+  * if the boundary block receives constant pressure boundary in z direction: input as dx, dy
+  kx = can be interchangeable depends on the boundary location
+  * if the boundary block receives constant pressure boundary in y direction: input as ky
+  * if the boundary block receives constant pressure boundary in x direction: input as kx
+  * if the boundary block receives constant pressure boundary in z direction: input as kz
+  dx = can be interchangeable depends on the boundary location
+  * if the boundary block receives constant pressure boundary in y direction: input as dy
+  * if the boundary block receives constant pressure boundary in x direction: input as dx
+  * if the boundary block receives constant pressure boundary in z direction: input as dz
+  mu = fluid viscosity in boundary block
+  B = fluid FVF in boundary block
+  rho = fluid density
+
+  Output:
+
+  qsc = Flow expression as string 
+  
+  e.g.: '0.757 (3000 - p(2,1) - 50)')  
+  where: 0.757 is the calculated transmissibility, 3000 is the p_b, (2,1)
+  is the no_block (boundary location), 50 is the potential term (γ * ∂Z/∂z)
+  """  
+  Ax = dy * dz
+  T = .001127 * (kx * Ax) / (mu * B * 0.5 * dx)
+
+  if bound_loc=='bottom':
+    Z = dz
+    gamma = .21584E-3 * rho * 32.174
+    Z_gamma = Z * gamma
+    qsc = '{} ({} - p{} - ({}))'.format(T, p_b, no_block, Z_gamma)
+  if bound_loc=='upper':
+    Z = -dz
+    gamma = .21584E-3 * rho * 32.174
+    Z_gamma = Z * gamma
+    qsc = '{} ({} - p{} - ({}))'.format(T, p_b, no_block, Z_gamma)
+  if bound_loc == 'west' or bound_loc == 'south':
+    # characterize as Left boundary
+    qsc = '{} ({} - p{})'.format(T, p_b, no_block) # Eq 4.45, zero (p_grad - 0) because Z1=Z2
+  if bound_loc == 'east' or bound_loc == 'north':
+    # characterize as Right boundary
+    qsc = '{} ({} - p{})'.format(T, p_b, no_block) # Eq 4.45, zero (p_grad - 0) because Z1=Z2
+  return(qsc)
+
 def constant_pressuregrad_bc1d(p_grad, dy, dz, kx, mu, B):
   """
   Flow expression for 1D constant pressure gradient boundary block
@@ -149,6 +203,61 @@ def constant_pressuregrad_bc2d(bound_loc, p_grad, dy, dz, kx, mu, B):
     # characterize as Left boundary
     qsc = '{}'.format(np.round(-(.001127 * (kx * Ax) / (mu * B)) * (p_grad - 0), 5)) # Eq 4.45, zero (p_grad - 0) because Z1=Z2
   if bound_loc == 'upper' or bound_loc == 'north' or bound_loc == 'east':
+    # characterize as Right boundary
+    qsc = '{}'.format(np.round((.001127 * (kx * Ax) / (mu * B)) * (p_grad - 0), 5)) # Eq 4.45, zero (p_grad - 0) because Z1=Z2
+  return(qsc)
+
+def constant_pressuregrad_bc3d(bound_loc, p_grad, dy, dz, kx, mu, B, rho):
+  """
+  Flow expression for 3D constant pressure gradient boundary block
+
+  (Actually, it's not discussed in Abou-Kassem book.) 
+  
+  Here, I still assume that for flow to either BOTTOM or UPPER boundary block, 
+  term (γ * ∂Z/∂z) is not 0. (∂Z/∂z) equals to 0.5*dz (half of grid height)
+  Except both BOTTOM or UPPER, term (γ * ∂Z/∂z) equals to 0.
+
+  Input:
+
+  bound_loc = location of boundary. Specify as 'west', 'east', 'south', 'north', 'bottom', or 'upper'
+  p_grad = pressure gradient at the boundary
+  dy, dz = these two can be interchangeable depends on the boundary location
+  * if the boundary block receives constant pressure boundary in y direction: input as dx, dz
+  * if the boundary block receives constant pressure boundary in x direction: input as dy, dz
+  * if the boundary block receives constant pressure boundary in z direction: input as dx, dy
+  kx = can be interchangeable depends on the boundary location
+  * if the boundary block receives constant pressure boundary in y direction: input as ky
+  * if the boundary block receives constant pressure boundary in x direction: input as kx
+  * if the boundary block receives constant pressure boundary in z direction: input as kz
+  mu = fluid viscosity in boundary block
+  B = fluid FVF in boundary block
+  rho = fluid density
+
+  Output:
+
+  qsc = Flow expression as string 
+  
+  e.g.: '100.5'
+  where: 100.5 is the calculated flow rate 
+  """
+
+  import numpy as np
+  
+  Ax = dy * dz
+  if bound_loc == 'bottom':
+    Z = 0.5 * dz
+    gamma = .21584E-3 * rho * 32.174
+    Z_gamma = Z * gamma
+    qsc = '{}'.format(np.round(-(.001127 * (kx * Ax) / (mu * B)) * (p_grad - Z_gamma), 5)) 
+  if bound_loc == 'upper':
+    Z = -0.5 * dz
+    gamma = .21584E-3 * rho * 32.174
+    Z_gamma = Z * gamma
+    qsc = '{}'.format(np.round((.001127 * (kx * Ax) / (mu * B)) * (p_grad - Z_gamma), 5)) 
+  if bound_loc == 'west' or bound_loc == 'south':
+    # characterize as Left boundary
+    qsc = '{}'.format(np.round(-(.001127 * (kx * Ax) / (mu * B)) * (p_grad - 0), 5)) # Eq 4.45, zero (p_grad - 0) because Z1=Z2
+  if bound_loc == 'east' or bound_loc == 'north':
     # characterize as Right boundary
     qsc = '{}'.format(np.round((.001127 * (kx * Ax) / (mu * B)) * (p_grad - 0), 5)) # Eq 4.45, zero (p_grad - 0) because Z1=Z2
   return(qsc)
