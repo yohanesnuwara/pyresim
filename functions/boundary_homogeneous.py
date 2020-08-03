8287ef148274b4984d458e9f472dadff417e25a2
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-08-04 00:26:12 +0700

    Update and rename boundary2.py to boundary_homogeneous.py

--- functions/boundary_homogeneous.py
+++ functions/boundary_homogeneous.py
@@ -4,7 +4,7 @@ Codes for boundary conditions
 @email: ign.nuwara97@gmail.com
 """
 
-def boundary_floweq1d(bound_type, dx, dy, dz, kx, mu, B, p_b=1000, p_grad=0.1, q_b=-200, no_block=1):
+def boundary_floweq1d(bound_type, dx, dy, dz, kx, mu, B, value, no_block=1):
     """
     Boundary Conditions for 1D Rectangular Reservoir (floweq simulation)
 
@@ -17,7 +17,7 @@ def boundary_floweq1d(bound_type, dx, dy, dz, kx, mu, B, p_b=1000, p_grad=0.1, q
     mu = fluid viscosity in boundary block
     B = fluid FVF in boundary block
 
-    p_b, p_grad, q_b = specify only one of these variables, depends on the boundary conditions
+    value = specify the boundary value, depends on the boundary conditions
     * if B.C. is 'constant_pressure', specify the pressure gradient: p_b
     * if B.C. is 'constant_pressuregrad', specify the pressure gradient: p_grad
     * if B.C. is 'constant_rate', specify the pressure gradient: q_b
@@ -55,21 +55,21 @@ def boundary_floweq1d(bound_type, dx, dy, dz, kx, mu, B, p_b=1000, p_grad=0.1, q
     if bound_type == 'constant_pressure':
         Ax = dy * dz
         T = .001127 * (kx * Ax) / (mu * B * 0.5 * dx)
-        qsc = '{} ({} - p{})'.format(T, p_b, no_block)
+        qsc = '{} ({} - p{})'.format(T, value, no_block)
 
     if bound_type == 'constant_pressuregrad':
         Ax = dy * dz
-        qsc = '{}'.format(np.round((.001127 * (kx * Ax) / (mu * B)) * (p_grad - 0), 5))  # Eq 4.45, zero (p_grad - 0) because Z1=Z2
+        qsc = '{}'.format(np.round((.001127 * (kx * Ax) / (mu * B)) * (value - 0), 5))  # Eq 4.45, zero (p_grad - 0) because Z1=Z2
 
     if bound_type == 'constant_rate':
-        qsc = '{}'.format(q_b)
+        qsc = '{}'.format(value)
 
     if bound_type == 'no_flow':
         qsc = 0
 
     return qsc
 
-def boundary_floweq2d(bound_type, bound_loc, dx, dy, dz, kx, mu, B, p_b=1000, p_grad=0.1, q_b=-200, no_block=(2,1), no_blocks_shared=4):
+def boundary_floweq2d(bound_type, bound_loc, dx, dy, dz, kx, ky, mu, B, value, no_block=(2,1), no_blocks_shared=4):
     """
     Boundary Conditions for 2D Rectangular Reservoir (floweq simulation)
 
@@ -79,11 +79,11 @@ def boundary_floweq2d(bound_type, bound_loc, dx, dy, dz, kx, mu, B, p_b=1000, p_
     'constant_pressuregrad', 'constant_rate', 'no_flow'
     bound_loc = location of boundary ('west', 'east', 'south', 'north')
     dx, dy, dz = size of grid
-    kx = permeability in x-direction
+    kx, ky = permeability in x-direction and y-direction
     mu = fluid viscosity in boundary block
     B = fluid FVF in boundary block
 
-    p_b, p_grad, q_b = specify only one of these variables, depends on the boundary conditions
+    value = specify the boundary value, depends on the boundary conditions
     * if B.C. is 'constant_pressure', specify the pressure gradient: p_b
     * if B.C. is 'constant_pressuregrad', specify the pressure gradient: p_grad
     * if B.C. is 'constant_rate', specify the pressure gradient: q_b
@@ -120,27 +120,40 @@ def boundary_floweq2d(bound_type, bound_loc, dx, dy, dz, kx, mu, B, p_b=1000, p_
     """
     import numpy as np
 
+    Ax = dy * dz; Ay = dx * dz
+
     if bound_type == 'constant_pressure':
-        Ax = dy * dz
-        T = .001127 * (kx * Ax) / (mu * B * 0.5 * dx)
-        qsc = '{} ({} - p{})'.format(T, p_b, no_block)
+        if bound_loc == 'east' or bound_loc == 'west':
+          T = .001127 * (kx * Ax) / (mu * B * 0.5 * dx)
+          qsc = '{} ({} - p{})'.format(T, value, no_block)
+        if bound_loc == 'south' or bound_loc == 'north':
+          T = .001127 * (ky * Ay) / (mu * B * 0.5 * dy)
+          qsc = '{} ({} - p{})'.format(T, value, no_block)
+
     if bound_type == 'constant_pressuregrad':
-        Ax = dy * dz
-        if bound_loc == 'south' or bound_loc == 'west':
-            # characterize as Left boundary
-            qsc = '{}'.format(np.round(-(.001127 * (kx * Ax) / (mu * B)) * (p_grad - 0), 5))  # Eq 4.45, zero (p_grad - 0) because Z1=Z2
-        if bound_loc == 'north' or bound_loc == 'east':
-            # characterize as Right boundary
-            qsc = '{}'.format(np.round((.001127 * (kx * Ax) / (mu * B)) * (p_grad - 0), 5))  # Eq 4.45, zero (p_grad - 0) because Z1=Z2
+        if bound_loc == 'west':
+          # characterize as Left boundary
+          qsc = '{}'.format(np.round(-(.001127 * (kx * Ax) / (mu * B)) * (value - 0), 5)) # Eq 4.45, zero (p_grad - 0) because Z1=Z2  
+        if bound_loc == 'south':
+          # characterize as Left boundary
+          qsc = '{}'.format(np.round(-(.001127 * (ky * Ay) / (mu * B)) * (value - 0), 5))             
+        if bound_loc == 'east':
+          # characterize as Right boundary
+          qsc = '{}'.format(np.round((.001127 * (kx * Ax) / (mu * B)) * (value - 0), 5))  
+        if bound_loc == 'north':
+          # characterize as Right boundary
+          qsc = '{}'.format(np.round((.001127 * (ky * Ay) / (mu * B)) * (value - 0), 5))  # Eq 4.45, zero (p_grad - 0) because Z1=Z2            
+    
     if bound_type == 'constant_rate':
-        qsc = (1 / no_blocks_shared) * q_b
+        qsc = (1 / no_blocks_shared) * value
+    
     if bound_type == 'no_flow':
         qsc = 0
     return(qsc)
 
-def boundary_floweq3d(bound_type, bound_loc, dx, dy, dz, kx, mu, B, rho, p_b=1000, p_grad=0.1, q_b=-200, no_block=(2,1,3), no_blocks_shared=4):
+def boundary_floweq3d(bound_type, bound_loc, dx, dy, dz, kx, ky, kz, mu, B, rho, value, no_block=(2,1,3), no_blocks_shared=4):
     """
-    Boundary Conditions for 2D Rectangular Reservoir (floweq simulation)
+    Boundary Conditions for 3D Rectangular Reservoir (floweq simulation)
 
     Input:
 
@@ -148,12 +161,12 @@ def boundary_floweq3d(bound_type, bound_loc, dx, dy, dz, kx, mu, B, rho, p_b=100
     'constant_pressuregrad', 'constant_rate', 'no_flow'
     bound_loc = location of boundary ('west', 'east', 'south', 'north')
     dx, dy, dz = size of grid
-    kx = permeability in x-direction
+    kx, ky, kz = permeability in x-direction, y-direction, z-direction
     mu = fluid viscosity in boundary block
     B = fluid FVF in boundary block
     rho = fluid density
 
-    p_b, p_grad, q_b = specify only one of these variables, depends on the boundary conditions
+    value = specify the boundary value, depends on the boundary conditions
     * if B.C. is 'constant_pressure', specify the pressure gradient: p_b
     * if B.C. is 'constant_pressuregrad', specify the pressure gradient: p_grad
     * if B.C. is 'constant_rate', specify the pressure gradient: q_b
@@ -165,7 +178,7 @@ def boundary_floweq3d(bound_type, bound_loc, dx, dy, dz, kx, mu, B, rho, p_b=100
     Input example: Bottom Boundary at grid block (5,4,1) has constant pressure of 2500 psia.
     Grid properties dx, dy, dz, kx, mu, B are known. There are 7 grids in the West.
 
-    > boundary_floweq2d('constant_pressure', 'bottom', dx=dx, dy=dx, dz=dz, kx=kx, mu=mu, B=B, rho=rho, p_grad=0.1, no_block=(5,4), no_blocks_shared=7)
+    > boundary_floweq3d('constant_pressure', 'bottom', dx=dx, dy=dx, dz=dz, kx=kx, mu=mu, B=B, rho=rho, p_grad=0.1, no_block=(5,4), no_blocks_shared=7)
 
     Output:
 
@@ -191,27 +204,30 @@ def boundary_floweq3d(bound_type, bound_loc, dx, dy, dz, kx, mu, B, rho, p_b=100
 
     import numpy as np
 
-    if bound_type == 'constant_pressure':
+    Ax = dy * dz 
+    Ay = dx * dz 
+    Az = dx * dy            
 
-        Ax = dy * dz
-        T = .001127 * (kx * Ax) / (mu * B * 0.5 * dx)
+    if bound_type == 'constant_pressure':
 
         if bound_loc == 'bottom':
             Z = dz
             gamma = .21584E-3 * rho * 32.174
             Z_gamma = Z * gamma
-            qsc = '{} ({} - p{} - ({}))'.format(T, p_b, no_block, Z_gamma)
+            T = .001127 * (kz * Az) / (mu * B * 0.5 * dz)            
+            qsc = '{} ({} - p{} - ({}))'.format(T, value, no_block, Z_gamma)
         if bound_loc == 'upper':
             Z = -dz
             gamma = .21584E-3 * rho * 32.174
             Z_gamma = Z * gamma
-            qsc = '{} ({} - p{} - ({}))'.format(T, p_b, no_block, Z_gamma)
-        if bound_loc == 'west' or bound_loc == 'south':
-            # characterize as Left boundary
-            qsc = '{} ({} - p{})'.format(T, p_b, no_block)  # Eq 4.45, zero (p_grad - 0) because Z1=Z2
-        if bound_loc == 'east' or bound_loc == 'north':
-            # characterize as Right boundary
-            qsc = '{} ({} - p{})'.format(T, p_b, no_block)  # Eq 4.45, zero (p_grad - 0) because Z1=Z2
+            T = .001127 * (kz * Az) / (mu * B * 0.5 * dz)
+            qsc = '{} ({} - p{} - ({}))'.format(T, value, no_block, Z_gamma)
+        if bound_loc == 'east' or bound_loc == 'west':
+          T = .001127 * (kx * Ax) / (mu * B * 0.5 * dx)
+          qsc = '{} ({} - p{})'.format(T, value, no_block)
+        if bound_loc == 'south' or bound_loc == 'north':
+          T = .001127 * (ky * Ay) / (mu * B * 0.5 * dy)
+          qsc = '{} ({} - p{})'.format(T, value, no_block)
 
 
     if bound_type == 'constant_pressuregrad':
@@ -221,23 +237,27 @@ def boundary_floweq3d(bound_type, bound_loc, dx, dy, dz, kx, mu, B, rho, p_b=100
             Z = 0.5 * dz
             gamma = .21584E-3 * rho * 32.174
             Z_gamma = Z * gamma
-            qsc = '{}'.format(np.round(-(.001127 * (kx * Ax) / (mu * B)) * (p_grad - Z_gamma), 5))
+            qsc = '{}'.format(np.round(-(.001127 * (kz * Az) / (mu * B)) * (value - Z_gamma), 5))
         if bound_loc == 'upper':
             Z = -0.5 * dz
             gamma = .21584E-3 * rho * 32.174
             Z_gamma = Z * gamma
-            qsc = '{}'.format(np.round((.001127 * (kx * Ax) / (mu * B)) * (p_grad - Z_gamma), 5))
-        if bound_loc == 'west' or bound_loc == 'south':
-            # characterize as Left boundary
-            qsc = '{}'.format(
-                np.round(-(.001127 * (kx * Ax) / (mu * B)) * (p_grad - 0), 5))  # Eq 4.45, zero (p_grad - 0) because Z1=Z2
-        if bound_loc == 'east' or bound_loc == 'north':
-            # characterize as Right boundary
-            qsc = '{}'.format(
-                np.round((.001127 * (kx * Ax) / (mu * B)) * (p_grad - 0), 5))  # Eq 4.45, zero (p_grad - 0) because Z1=Z2
-
+            qsc = '{}'.format(np.round((.001127 * (kz * Az) / (mu * B)) * (value - Z_gamma), 5))
+        if bound_loc == 'west':
+          # characterize as Left boundary
+          qsc = '{}'.format(np.round(-(.001127 * (kx * Ax) / (mu * B)) * (value - 0), 5))  # Eq 4.45, zero (p_grad - 0) because Z1=Z2
+        if bound_loc == 'south':
+          # characterize as Left boundary
+          qsc = '{}'.format(np.round(-(.001127 * (ky * Ay) / (mu * B)) * (value - 0), 5))  # Eq 4.45, zero (p_grad - 0) because Z1=Z2            
+        if bound_loc == 'east':
+          # characterize as Right boundary
+          qsc = '{}'.format(np.round((.001127 * (kx * Ax) / (mu * B)) * (value - 0), 5))  # Eq 4.45, zero (p_grad - 0) because Z1=Z2
+        if bound_loc == 'north':
+          # characterize as Right boundary
+          qsc = '{}'.format(np.round((.001127 * (ky * Ay) / (mu * B)) * (value - 0), 5))  # Eq 4.45, zero (p_grad - 0) because Z1=Z2            
+    
     if bound_type == 'constant_rate':
-        qsc = (1 / no_blocks_shared) * q_b
+        qsc = (1 / no_blocks_shared) * value
 
     if bound_type == 'no_flow':
         qsc = 0
