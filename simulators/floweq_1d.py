a9f1820ee19d20211ff2828eeb5217aeb490181b
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-08-04 00:31:52 +0700

    Update floweq_1d.py

--- simulators/floweq_1d.py
+++ simulators/floweq_1d.py
@@ -1,12 +1,20 @@
 """
-Simulation in 1D Rectangular Reservoir Grids 
+Simulation in 1D Homogeneous Rectangular Reservoir Grids 
 
 @author: Yohanes Nuwara
 @email: ign.nuwara97@gmail.com
 """
 
-# blocks location
-block = np.arange(1, 5+1)
+from boundary_homogeneous import *
+from transmissibility_homogeneous import *
+from utilities_homogeneous import *
+
+"""""""""""
+INPUT
+"""""""""""
+
+# number of blocks
+xi = 5
 
 # define parameters, in this case blocks are homogeneous and same in size
 dx = 1000 # ft
@@ -16,43 +24,43 @@ kx = 15 # md
 B = 1 # RB/STB
 mu = 10 # cp
 
-# assign all params to all blocks
-dx = np.full(len(block), dx)
-dy = np.full(len(block), dy)
-dz = np.full(len(block), dz)
-kx = np.full(len(block), kx)
-B = np.full(len(block), B)
-mu = np.full(len(block), mu)
-
 # source term
-qsc = np.zeros(len(block)) # initiate with zeros
-qsc[3] = -150 # inject the source to block 4
+xsc = np.array([4]) # location of block where production / injection well is located
+q = np.array([-150]) # flow rate of production / injection
 
 # boundary conditions
 p_bW = 5000 # pressure at left (West) boundary
 q_bE = 0. # flow rate at right (East) boundary
 
-print('Left-Hand Side (RHS) of Flow equation in each boundary block')
-print('1st term: Flow in X-direction from adjacent block')
-print('2nd term: Fictitious flow into boundary block')
-print('3rd term: Source accumulation \n')
+"""""""""""
+MAIN CODE
+"""""""""""
 
-print('Left-Hand Side (RHS) of Flow equation in each internal block')
-print('1st term: Flow in X-direction from previous block')
-print('2nd term: Flow in X-direction from next block')
+" Create property grids "
+# blocks location
+block = np.arange(1, xi+1)
+
+# source block (production or injection well)
+qsc = source1d(q, xsc, xi) # call function HERE
+
+" Produce flow equation "
+
+print('Left-Hand Side (RHS) of Flow equation in each grid block')
+print('1st term: Flow in X-direction from next block (Tx+)')
+print('2nd term: Flow in X-direction from previous block (Tx-)')
 print('3rd term: Source accumulation \n')
 
 for i in range(len(block)):
-  A = dy[i] * dz[i]
-  T_min = .001127 * (kx[i] * A) / (mu[i] * B[i] * dx[i])
-  T_plus = T_min 
+
+  T_min, T_plus = transmissibility1d(dx, dy, dz, kx, mu, B)
+
   if i == 0:
     # constant pressure at left boundary
-    qsc_b = constant_pressure_bc1d(block[i], p_bW, dy[i], dz[i], kx[i], dx[i], mu[i], B[i])
+    qsc_b = boundary_floweq1d('constant_pressure', dx, dy, dz, kx, mu, B, value=p_bW, no_block=block[i])
     print('Boundary Block {}: {} (p{} - p{}) + {} + {}'.format(block[i], T_plus, block[i+1], block[i], qsc_b, qsc[i]))
   elif i == len(block)-1:
     # no flow at right boundary
-    qsc_b = constant_rate_bc1d(q_bE)
+    qsc_b = boundary_floweq1d('constant_rate', dx, dy, dz, kx, mu, B, value=q_bE, no_block=block[i])
     print('Boundary Block {}: {} (p{} - p{}) + {} + {}'.format(block[i], T_min, block[i-1], block[i], qsc_b, qsc[i]))
   else:
     print('Interior Block {}: {} (p{} - p{}) + {} (p{} - p{}) + {}'.format(block[i], T_min, block[i-1], block[i], T_plus, block[i+1], block[i], qsc[i]))
