5d96cc7fa010bf2c2fb3e6ed3858e967f978e07f
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-08-13 20:50:22 +0700

    Update gridding.py

--- functions/gridding.py
+++ functions/gridding.py
@@ -17,3 +17,30 @@ def fill_active_blocks(prop_in, x):
   prop_out = prop_out.T
   
   return prop_out
+
+def source2d(q, xysc, xi, yi):
+  """
+  Create source grid for 2D rectangular reservoir
+
+  Input:
+
+  q = amount of production / injection rate (+ value for injection, - value for
+  production)
+  xysc = location of production / injection well (x,y)
+  xi, yi = dimension in x-direction and y-direction
+
+  Output:
+
+  qsc = source grid (1D numpy array), informs source value at each grid
+  """    
+  import numpy as np
+  xsc = xysc[:,0] - 1 
+  ysc = xysc[:,1] - 1
+
+  # initiate with zeros.
+  qsc = np.array([[0.]*yi]*xi)
+
+  # replace the zeros at coordinate of source term, with the rate of source term
+  for i, j, k in zip(xsc, ysc, range(len(q))):
+    qsc[i][j] = q[k]    
+  return qsc
