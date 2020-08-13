86e50d5a2cd4a6cdd4afcc254fc0e24c76541b4a
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-08-13 20:10:21 +0700

    Update transmissibility.py

--- functions/transmissibility.py
+++ functions/transmissibility.py
@@ -1,75 +1,28 @@
-# calculate transmissibility of cylindrical grid (geom factor from Table 4-3, Abou-Kassem p. 99)
+def transmissibility2d_boundary(bound_loc, bound_type, dx, dy, dz, kx, mu, B):
+  """
+  Calculate the transmissibility at boundary (specifically for constant pressure B.C.)
+  2D reservoir
+  """
+  if bound_type=='constant_pressure':
+    if bound_loc==1 or bound_loc==2:
+      Ax = dy * dz
+      T = .001127 * (kx * Ax) / (mu * B * 0.5 * dx)
+    if bound_loc==3 or bound_loc==4:
+      Ay = dx * dz
+      T = .001127 * (ky * Ay) / (mu * B * 0.5 * dy)      
+  if bound_type=='constant_pressuregrad' or bound_type=='constant_rate' or bound_type=='no_flow':
+    T = 0    
+  return T 
 
-def trans_r_min(dz_now, dz_prev, dtheta, alpha_tg, kh_now, kh_prev, mu, B):
-  """
-  Calculate transmissibility of cylindrical grid in r- direction
-  """
-  
-  import numpy as np
-  
-  # transmissibility geometric factor
-  G_trans_r_min = (.001127 * dtheta) / (np.log(alpha_tg * np.log(alpha_tg) / (alpha_tg - 1)) / (dz_now * kh_now) + np.log((alpha_tg - 1) / np.log(alpha_tg)) / (dz_prev * kh_prev))
-  # calculate transmissibility
-  T_r_min = G_trans_r_min / (mu * B)
-  return T_r_min
+def transmissibility2d(dx, dy, dz, kx, ky, mu, B):
+  Ax = dy * dz
+  Ay = dx * dz
 
-def trans_r_plus(dz_now, dz_next, dtheta, alpha_tg, kh_now, kh_next, mu, B):
-  """
-  Calculate transmissibility of cylindrical grid in r+ direction
-  """
-  
-  import numpy as np
-  
-  # transmissibility geometric factor
-  G_trans_r_plus = (.001127 * dtheta) / (np.log((alpha_tg - 1) / np.log(alpha_tg)) / (dz_now * kh_now) + np.log(alpha_tg * np.log(alpha_tg) / (alpha_tg - 1)) / (dz_next * kh_next))
-  # calculate transmissibility
-  T_r_plus = G_trans_r_plus / (mu * B)
-  return T_r_plus
+  # flow to x direction
+  Tx_min = .001127 * (kx * Ax) / (mu * B * dx)
+  Tx_plus = Tx_min
 
-def trans_z_min(dz_now, dz_prev, dv_now, kz_now, kz_prev, mu, B):
-  """
-  Calculate transmissibility of cylindrical grid in z- direction
-  """
-  
-  # transmissibility geometric factor
-  G_trans_z_min = (2 * .001127 * (dv_now / dz_now)) / ((dz_now / kz_now) + (dz_prev / kz_prev))
-  # calculate transmissibility
-  T_z_min = G_trans_z_min / (mu * B)
-  return T_z_min
-
-def trans_z_plus(dz_now, dz_next, dv_now, kz_now, kz_next, mu, B):
-  """
-  Calculate transmissibility of cylindrical grid in z+ direction
-  """
-  
-  # transmissibility geometric factor
-  G_trans_z_plus = (2 * .001127 * (dv_now / dz_now)) / ((dz_now / kz_now) + (dz_next / kz_next))
-  # calculate transmissibility
-  T_z_plus = G_trans_z_plus / (mu * B)
-  return T_z_plus
-
-def trans_theta_min(dz_now, dz_prev, alpha_tg, dtheta_now, dtheta_prev, ktheta_now, ktheta_prev, mu, B):
-  """
-  Calculate transmissibility of cylindrical grid in θ- direction
-  """
-  
-  import numpy as np
-  
-  # transmissibility geometric factor
-  G_trans_theta_min = (2 * .001127 * np.log(alpha_tg)) / ((dtheta_now / (dz_now * ktheta_now)) + (dtheta_prev / (dz_prev * ktheta_prev)))
-  # calculate transmissibility
-  T_theta_min = G_trans_theta_min / (mu * B)
-  return T_theta_min
-
-def trans_theta_plus(dz_now, dz_next, alpha_tg, dtheta_now, dtheta_next, ktheta_now, ktheta_next, mu, B):
-  """
-  Calculate transmissibility of cylindrical grid in θ+ direction
-  """
-  
-  import numpy as np
-  
-  # transmissibility geometric factor
-  G_trans_theta_plus = (2 * .001127 * np.log(alpha_tg)) / ((dtheta_now / (dz_now * ktheta_now)) + (dtheta_next / (dz_next * ktheta_next)))
-  # calculate transmissibility
-  T_theta_plus = G_trans_theta_plus / (mu * B)
-  return T_theta_plus
+  # flow to y direction
+  Ty_min = .001127 * (ky * Ay) / (mu * B * dy)
+  Ty_plus = Ty_min   
+  return Tx_min, Tx_plus, Ty_min, Ty_plus
